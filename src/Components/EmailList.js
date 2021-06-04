1db11a22452af5e749a5e5c5776e5e87f00a52c0
trackrise <75917678+trackrise@users.noreply.github.com>
2021-06-03 18:06:19 -0700

    Got search bar working. Sending an email mostly works, but cannot get anything other than a 400 no matter how I try to send it. I'm stuck!

--- src/Components/EmailList.js
+++ src/Components/EmailList.js
@@ -10,6 +10,7 @@ class EmailList extends React.Component {
 
     this.displayMessage = this.displayMessage.bind(this);
     this.writeEmail = this.writeEmail.bind(this);
+    this.search = this.search.bind(this);
   }
 
 
@@ -38,16 +39,60 @@ class EmailList extends React.Component {
   }
 
   writeEmail() {
-        console.log('test')
       this.setState({emailMessage: 
-      <form>
-          <label>Sender:<input type="text"></input></label>
-          <label>Recepient:<input type="text"></input></label>
-          <label>Subject:<input type="text"></input></label>
-          <label>Message:<textarea cols="30" rows="10"></textarea></label>
-          <label>Date:<input type="date"></input></label>
-          <button type="submit">Send Email</button>
+      <form className="createEmail">
+          <label>Sender:<input type="email" name="sender" className="sender" defaultValue="test@gmail.com"></input></label>
+          <label>recipient:<input type="email" name="recipient" className="recipient" defaultValue="test2@gmail.com"></input></label>
+          <label>Subject:<input type="text" name="subject" className="subject" defaultValue="subject here"></input></label>
+          <label><textarea cols="30" rows="10" name="message" className="message" defaultValue="message here"></textarea></label>
+          <button type="button" onClick={this.sendEmail}>Send Email</button>
+
         </form>})
+  }
+
+  async sendEmail() {
+
+    const url = 'http://localhost:3001/emails'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    let newId = data[data.length - 1].id;
+    newId++;
+
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+
+    let dataToSend = 
+    {
+        sender: document.querySelector('.sender').value,
+        recipient: document.querySelector('.recipient').value,
+        subject: document.querySelector('.subject').value,
+        message: document.querySelector('.message').value,
+        date: today.toISOString(),
+        id: newId
+    } 
+
+    const send = await fetch('http://localhost:3001/send', {
+        method: "POST",
+        body: JSON.stringify(dataToSend)  
+    })
+
+    const sentData = await send.json()
+
+    console.log(JSON.stringify(dataToSend))
+    console.log(sentData)
+  }
+
+  async search() {
+      let searchBar = document.querySelector('.searchBar')
+      const response = await fetch(`http://localhost:3001/search?query=${searchBar.value}`)
+      const data = await response.json()
+      this.setState({emailList: data.map((email) => {
+
+    return (<div className="emailItem" id={email.id} onClick={this.displayMessage}>
+                <span className ="emailSubject">SUBJECT: {email.subject}</span>
+                <span>SENDER: {email.sender}</span>
+                </div>)})})
   }
 
   render() {
@@ -56,8 +101,8 @@ class EmailList extends React.Component {
         <div className="header">
             <span>Yes, this is the actual Gmail</span>
             <button type="button" onClick={this.writeEmail}>Create Email</button>
-            <span><input type="text"></input>
-            <button>Search</button></span>
+            <span><form><input type="text" name="query" className="searchBar"></input>
+            <button type="button" onClick={this.search}>Search</button></form></span>
         </div>
         <div className="emailList App-header">{this.state.emailList}</div>
         <div className="emailArea App">{this.state.emailMessage}</div>
